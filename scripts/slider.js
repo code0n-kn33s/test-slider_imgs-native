cac4be37c2bb6217e9d4f2643c6aafc791bcf34a
‘Alex’ <oleksii.bashinskii@mirs.com>
2024-12-10 13:25:55 +0200

    little fix

--- scripts/slider.js
+++ scripts/slider.js
@@ -16,8 +16,20 @@ document.addEventListener('DOMContentLoaded', () => {
           if (index === slides.length - 1) slide.classList.add('prev');
 
           slide.innerHTML = `
-            <img class="lazy" data-src="${slideData.image}" alt="Slide ${index + 1}">
-            <div class="item-description">${slideData.content}</div>
+
+              <img class="lazy" data-src="${slideData.image}" alt="Slide ${index + 1}">
+              <div class="slider-item-footer">
+                <div class="slider-item-text">
+                  <div class="slider-item-title">${slideData.title}</div>
+                  <div class="slider-item-description">${slideData.content}</div>
+                </div>
+                <div class="slider-item-button">  
+                  <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M13.6943 3.33095L1.53124 3.33095L1.53124 0.498047L18.5307 0.498048L18.5307 17.4975L15.6978 17.4975L15.6978 5.33442L2.50292 18.5293L0.499451 16.5258L13.6943 3.33095Z" />
+                  </svg>
+                </div>
+              </div>
+
           `;
           sliderList.appendChild(slide);
       });
